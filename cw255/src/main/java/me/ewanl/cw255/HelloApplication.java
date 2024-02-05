ab13449056cbff58fda15b9f92562fb65ce53b99
ewanlew <ewann.lew@gmail.com>
2024-02-05 15:34:05 +0000

    Styling css

--- cw255/src/main/java/me/ewanl/cw255/HelloApplication.java
+++ cw255/src/main/java/me/ewanl/cw255/HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("photoshop-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setResizable(false);
+        stage.setTitle("Photoshop Lite");
         stage.setScene(scene);
         stage.show();
     }
--- cw255/src/main/java/me/ewanl/cw255/PhotoshopController.java
+++ cw255/src/main/java/me/ewanl/cw255/PhotoshopController.java
@@ -1,9 +1,12 @@
 package me.ewanl.cw255;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
 public class PhotoshopController {
     public ToggleGroup interpolation;
     public RadioButton btnNearestNeighbour;
+    public Label lblGamma;
+    public Label lblGammaValue;
 }
